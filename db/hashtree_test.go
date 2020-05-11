52f3140c3f727ec7e259f2e1e06ef9abb69fff2f
charleygrossman <charleygrossman@gmail.com>
2020-05-11 18:03:44 -0500

    WIP: Test insertion into balanced hash tree maintains binary search property.

--- db/hashtree_test.go
+++ db/hashtree_test.go
@@ -1,11 +1,12 @@
 // TODO: Testing
-//  - Every transaction inserted into the tree increases tree size by 1.
+//  x Every transaction inserted into the tree increases tree size by 1.
 //  - Binary tree property maintained every insertion.
 //  - Insertion ids of nodes follow binary tree property.
 //  - Maintains logarithmic height every insertion.
 //  - No right-leaning red links after insertion.
 //  - No two adjacent left-leaning red links after insertion.
 //  - All black link paths from root to leaf have same length.
+//  - Benchmark tests for runtime of operations.
 package db
 
 import (
@@ -31,6 +32,8 @@ func TestMain(m *testing.M) {
 	m.Run()
 }
 
+// TestInsertIncrementsSize asserts that every insertion into a tree
+// increases its size, or number of nodes, by one.
 func TestInsertIncrementsSize(t *testing.T) {
 	tree := &Tree{}
 
@@ -40,15 +43,49 @@ func TestInsertIncrementsSize(t *testing.T) {
 		expected := uint64(i + 1)
 		if actual := tree.Size; expected != actual {
 			t.Errorf("tree size: expected: %d actual: %d", expected, actual)
-		} else {
-			t.Logf("tree size: %d", actual)
 		}
 	}
 }
 
-// TestNodeInsertionIds asserts that insertion ids of nodes
-// (the node.id uuid field) maintain the binary tree invariant
-// every insertion.
-func TestNodeInsertionIds(t *testing.T) {
+// TestInsertMaintainsBinarySearchProperty asserts that insertion into a tree
+// maintains the binary search tree property; that is, the insertion id of
+// the root of any subtree is greater than its left child, and less than its right.
+func TestInsertMaintainsBinarySearchProperty(t *testing.T) {
+	tree := &Tree{}
 
+	for i := 0; i < 100; i++ {
+		tree.Insert(&testTxn{})
+
+		if ok := traverse(tree.Root, func(n *node) bool {
+			if n != nil {
+				l, r := n.leftP, n.rightP
+				if l != nil {
+					if n.id.String() <= l.id.String() {
+						return false
+					}
+				}
+				if r != nil {
+					if n.id.String() >= r.id.String() {
+						return false
+					}
+				}
+			}
+			return true
+		}); !ok {
+			t.FailNow()
+		}
+	}
+}
+
+// traverse recursively traverses the tree from the provided node,
+// terminating early and returning false if the provided predicate
+// ever evaluates to false.
+func traverse(n *node, predicate func(*node) bool) bool {
+	if !predicate(n) {
+		return false
+	}
+	if n != nil && (!traverse(n.leftP, predicate) || !traverse(n.rightP, predicate)) {
+		return false
+	}
+	return true
 }
