8f227bdec7c83eabe3acb48537973f07ca8ff9b8
charleygrossman <charleygrossman@gmail.com>
2021-02-15 17:42:24 -0600

    market: trader node request and response queues

--- market/graph.go
+++ market/graph.go
@@ -1,6 +1,7 @@
 package market
 
 import (
+	"container/list"
 	"errors"
 	"github.com/google/uuid"
 )
@@ -22,10 +23,11 @@ func NewGraph(nodes []Node, adjacencies map[uuid.UUID][]uuid.UUID) *Graph {
 		m[n.Trader.ID] = &Node{
 			Trader:         n.Trader,
 			GraphID:        n.GraphID,
-			TradeRequests:  make(chan TradeMessage),
-			TradeResponses: make(chan TradeMessage),
+			TradeRequests:  list.New(),
+			TradeResponses: list.New(),
 		}
 	}
+	g.node = m
 	return g
 }
 
@@ -36,7 +38,7 @@ func (g Graph) Adjacent(graphID, traderID uuid.UUID) ([]uuid.UUID, error) {
 	return g.adjacent[traderID], nil
 }
 
-func (g Graph) SendTradeMessage(graphID uuid.UUID, message TradeMessage) error {
+func (g Graph) SendTradeRequest(graphID uuid.UUID, message TradeMessage) error {
 	if err := g.authorizeTradeMessage(graphID, message); err != nil {
 		return err
 	}
@@ -44,8 +46,62 @@ func (g Graph) SendTradeMessage(graphID uuid.UUID, message TradeMessage) error {
 	if !ok || n == nil || n.Trader.ID != message.ToTraderID {
 		return errors.New("receiving trader not found")
 	}
-	n.TradeRequests <- message
+	n.TradeRequests.PushBack(message)
 	return nil
+}
+
+func (g Graph) SendTradeResponse(graphID uuid.UUID, message TradeMessage) error {
+	if err := g.authorizeTradeMessage(graphID, message); err != nil {
+		return err
+	}
+	n, ok := g.node[message.ToTraderID]
+	if !ok || n == nil || n.Trader.ID != message.ToTraderID {
+		return errors.New("receiving trader not found")
+	}
+	n.TradeResponses.PushBack(message)
+	return nil
+}
+
+func (g Graph) TradeRequests(graphID uuid.UUID, traderID uuid.UUID) ([]TradeMessage, error) {
+	if err := g.authorizeTrader(graphID, traderID); err != nil {
+		return nil, err
+	}
+	n, ok := g.node[traderID]
+	if !ok || n == nil || n.Trader.ID != traderID {
+		return nil, errors.New("trader not found")
+	}
+	requests := make([]TradeMessage, n.TradeRequests.Len())
+	i := 0
+	for e := n.TradeRequests.Front(); e != nil; e = e.Next() {
+		msg, ok := e.Value.(TradeMessage)
+		if !ok {
+			return nil, errors.New("invalid trade request type")
+		}
+		requests[i] = msg
+		i++
+	}
+	return requests, nil
+}
+
+func (g Graph) TradeResponses(graphID uuid.UUID, traderID uuid.UUID) ([]TradeMessage, error) {
+	if err := g.authorizeTrader(graphID, traderID); err != nil {
+		return nil, err
+	}
+	n, ok := g.node[traderID]
+	if !ok || n == nil || n.Trader.ID != traderID {
+		return nil, errors.New("trader not found")
+	}
+	responses := make([]TradeMessage, n.TradeResponses.Len())
+	i := 0
+	for e := n.TradeResponses.Front(); e != nil; e = e.Next() {
+		msg, ok := e.Value.(TradeMessage)
+		if !ok {
+			return nil, errors.New("invalid trade response type")
+		}
+		responses[i] = msg
+		i++
+	}
+	return responses, nil
 }
 
 func (g Graph) authorizeTrader(graphID, traderID uuid.UUID) error {
@@ -72,8 +128,8 @@ func (g Graph) authorizeTradeMessage(graphID uuid.UUID, message TradeMessage) er
 type Node struct {
 	Trader         *Trader
 	GraphID        uuid.UUID
-	TradeRequests  chan TradeMessage
-	TradeResponses chan TradeMessage
+	TradeRequests  *list.List
+	TradeResponses *list.List
 }
 
 type Edge struct {
--- market/trade.go
+++ market/trade.go
@@ -1,7 +1,6 @@
 package market
 
 import (
-	"fmt"
 	"github.com/google/uuid"
 )
 
@@ -12,23 +11,70 @@ type Trader struct {
 	Wants     []Item
 }
 
-func (t Trader) SendTradeMessage(g *Graph, toTraderID uuid.UUID) error {
+func (t Trader) SendTradeRequests(g *Graph) error {
 	adjacent, err := g.Adjacent(t.GraphID, t.ID)
 	if err != nil {
 		return err
 	}
 	for _, adjTraderID := range adjacent {
-		if adjTraderID == toTraderID {
+		msg := TradeMessage{
+			FromTraderID: t.ID,
+			ToTraderID:   adjTraderID,
+			Tradable:     t.Inventory,
+			Wants:        t.Wants,
+		}
+		if err := g.SendTradeRequest(t.GraphID, msg); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (t Trader) SendTradeResponses(g *Graph) error {
+	requests, err := g.TradeRequests(t.GraphID, t.ID)
+	if err != nil {
+		return err
+	}
+	for _, r := range requests {
+		if t.acceptRequest(r) {
 			msg := TradeMessage{
 				FromTraderID: t.ID,
-				ToTraderID:   toTraderID,
+				ToTraderID:   r.FromTraderID,
 				Tradable:     t.Inventory,
 				Wants:        t.Wants,
 			}
-			return g.SendTradeMessage(t.GraphID, msg)
+			if err := g.SendTradeResponse(t.GraphID, msg); err != nil {
+				return err
+			}
 		}
 	}
-	return fmt.Errorf("trader not found: %s", toTraderID.String())
+	return nil
+}
+
+func (t Trader) acceptRequest(request TradeMessage) bool {
+	return t.hasRequestWant(request) && t.wantsRequestTradable(request)
+}
+
+func (t Trader) hasRequestWant(request TradeMessage) bool {
+	for _, item := range t.Inventory {
+		for _, v := range request.Wants {
+			if item == v {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (t Trader) wantsRequestTradable(request TradeMessage) bool {
+	for _, item := range t.Wants {
+		for _, v := range request.Tradable {
+			if item == v {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 type TradeMessage struct {
